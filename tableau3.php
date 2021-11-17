63c2d5faf03b473ac435ce91d7ce28649b8e0672
ycherifi <yacine.cherifi@esme.fr>
2021-11-17 10:46:04 +0100

    Third commit

---
+++ tableau3.php
@@ -0,0 +1,6 @@
+<?php
+         $marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
+         $i=0;
+         foreach ($elem as $marque) {
+               echo "Vente de : " . $elem . "<br/>";
+} ?>
